7ea9b9de99a08f57fe5f759e56b46be0d1aa9c7f
MarvelShubhamIITM <165567256+MarvelShubhamIITM@users.noreply.github.com>
2024-04-24 20:31:12 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,3 +2,7 @@
 
 export GITHUB_USERNAME="MarvelShubhamIITM"
 export GITHUB_REPOSITORY="se2001-22f1001934"
+git add script.sh
+git commit -m " added script.sh
+git push origin main
+mv ~/se2001/assignment_4/se2001-<roll-number>/script.sh ~/se2001/assignment_4
